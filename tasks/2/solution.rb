365a5cc7e4aafd838907a4bbc2c0d490ced0b2d3
kalisb <kalisb@abv.bg>
2017-01-15 11:40:15 +0200

    Clean up the code.
    Use map instead of each where appropriate.

--- tasks/2/solution.rb
+++ tasks/2/solution.rb
@@ -2,43 +2,31 @@ class Hash
   def fetch_deep(path)
     path_elements = path.split('.')
     path_elements.reduce(self) do |hsh, key|
-      if hsh
-        hsh = hsh.stringify_keys
-        hsh[key]
-      end
+      hsh.stringify_keys[key] if hsh
     end
   end
 
   def reshape(shape)
-    result = shape
-    shape.each do |(key, value)|
-      if value.is_a? Hash
-        reshape(value)
-      else
-        result[key] = self.fetch_deep(value)
-      end
-    end
+    return fetch_deep(shape) if shape.is_a? String
+    shape.map do |new_key, shape|
+      [new_key, reshape(shape)]
+    end.to_h
   end
 
   def stringify_keys
-    result = {}
-    each_key do |key|
-      result[key.to_s] = self[key]
-    end
-    result
+    map do |key, value|
+      [key.to_s, value]
+    end.to_h
   end
 end
 class Array
   def reshape(shape)
-    self.each_with_index do |value, index|
-      self[index] = value.reshape(shape.dup)
-    end
+    map { |value| value.reshape(shape) }
   end
 
   def stringify_keys
-    result = self.each_with_index.map do |value, index|
+    self.each_with_index.map do |value, index|
       [index.to_s, value]
-    end
-    result.to_h
+    end.to_h
   end
 end
